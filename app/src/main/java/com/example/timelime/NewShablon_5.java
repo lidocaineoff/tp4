c853936a5dd1b4a1b5b727920d463a26352a0836
Valentina Gordeeva <gordeevalentina@mail.ru>
2023-04-13 12:46:55 +0700

    I made a reverse timer for 10 minutes, with the ability to stop, start and reset back to 10 minutes

--- app/src/main/java/com/example/timelime/NewShablon_5.java
+++ app/src/main/java/com/example/timelime/NewShablon_5.java
@@ -10,5 +10,7 @@ public class NewShablon_5 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_shablon5);
+
+
     }
 }
