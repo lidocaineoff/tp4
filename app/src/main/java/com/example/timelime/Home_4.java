56af4f7fd1399cb0469d90ed40fbb62a0c7c2dc0
Aspire3 <samoilovali100@gmail.com>
2023-04-18 23:20:09 +0700

    menu forma and gravity

--- app/src/main/java/com/example/timelime/Home_4.java
+++ app/src/main/java/com/example/timelime/Home_4.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
@@ -26,6 +29,8 @@ public class Home_4 extends AppCompatActivity {
 
                 Dialog dialog = new Dialog(Home_4.this);
                 dialog.setContentView(R.layout.dialog_menu);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().setGravity(Gravity.RIGHT | Gravity.TOP);
 
 
             /*
